8e8ab7b771f58408a88541f0d3f30e25369e9dc0
olegg789 <olegchikelev789789@gmail.com>
2022-04-12 21:13:06 +0300

    Updates

--- precache-manifest.d3aa836bcc943ebbd2cc8e4acec3b0c0.js
+++ precache-manifest.d3aa836bcc943ebbd2cc8e4acec3b0c0.js
@@ -8,8 +8,8 @@ self.__precacheManifest = [
     "url": "./static/js/main.3590c7dd.chunk.js"
   },
   {
-    "revision": "9ad855ce368dfe7819ba",
-    "url": "./static/js/runtime~main.050ebad1.js"
+    "revision": "9326e33791e252a97ea8",
+    "url": "./static/js/runtime~main.1cb3d6cf.js"
   },
   {
     "revision": "fca1582abd2a562ac2e7",
@@ -20,15 +20,15 @@ self.__precacheManifest = [
     "url": "./static/js/2.1b3a3411.chunk.js"
   },
   {
-    "revision": "d597b5088654f25cd356",
-    "url": "./static/js/3.d077807f.chunk.js"
+    "revision": "6395540b4ca906367cf1",
+    "url": "./static/js/3.6f0ac0f5.chunk.js"
   },
   {
     "revision": "c257d6b6f659217ee8b1",
     "url": "./static/js/4.3ba8b6d3.chunk.js"
   },
   {
-    "revision": "774380169fb2deb210a3bb35b56307ed",
+    "revision": "56269b536576d3ba9b5d3b4ce905c586",
     "url": "./index.html"
   }
 ];
